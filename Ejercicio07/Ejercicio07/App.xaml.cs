2fff441e922114f553131aab2a1cf929a0c84978
reynaldo29 <acm19@outlook.com>
2022-11-01 14:50:15 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio07/Ejercicio07/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio07
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new PickerDemo();
+            //MainPage = new ListViewDemo();
+            //MainPage = new BindingModeDemo();
+            MainPage = new ViewToViewDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
